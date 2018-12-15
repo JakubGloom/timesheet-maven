307f6f067753e5671c2928b3719f36f826f92a51
Sasenku <darkman@plusnet.pl>
2018-12-15 21:27:28 +0100

    Added properties to Employee class

--- src/main/java/com/gluma/timesheet/datamdodel/Employee.java
+++ src/main/java/com/gluma/timesheet/datamdodel/Employee.java
@@ -1,123 +1,147 @@
 package com.gluma.timesheet.datamdodel;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Employee{
 
-    private int idEmployee;
-    private String name;
-    private String surname;
-    private String login;
-    private String password;
-    private String access;
-    private int seed;
-    private String fullName;
+    private IntegerProperty idEmployee = new SimpleIntegerProperty();
+    private StringProperty name = new SimpleStringProperty();
+    private StringProperty surname = new SimpleStringProperty();
+    private StringProperty login = new SimpleStringProperty();
+    private StringProperty password = new SimpleStringProperty();
+    private StringProperty access = new SimpleStringProperty();
+    private IntegerProperty seed = new SimpleIntegerProperty();
+    private  StringProperty fullName = new SimpleStringProperty();
 
     public static Employee loggedEmployee;
 
     public Employee(){}
     public Employee(int idEmployee, String name, String surname, String login, String password) {
-        this.idEmployee = idEmployee;
-        this.name = name;
-        this.surname = surname;
-        this.login = login;
-        this.password = password;
+        this.idEmployee.set(idEmployee);
+        this.name.set(name);
+        this.surname.set(surname);
+        this.login.set(login);
+        this.password.set(password);
     }
 
     public Employee(int idEmployee, String name, String surname, String login, String password, String access) {
-        this.idEmployee = idEmployee;
-        this.name = name;
-        this.surname = surname;
-        this.login = login;
-        this.password = password;
-        this.access = access;
+        this.idEmployee.set(idEmployee);
+        this.name.set(name);
+        this.surname.set(surname);
+        this.login.set(login);
+        this.password.set(password);
+        this.access.set(access);
     }
 
     public Employee(int idEmployee, String name, String surname) {
-        this.idEmployee = idEmployee;
-        this.name = name;
-        this.surname = surname;
-        this.fullName = name + " " + surname;
+        this.idEmployee.set(idEmployee);
+        this.name.set(name);
+        this.surname.set(surname);
+        this.fullName.set(name.concat(" ".concat(surname)));
     }
 
     public Employee(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
-        this.fullName = name + " " + surname;
+        this.name.set(name);
+        this.surname.set(surname);
+        this.fullName.set(name.concat(" ".concat(surname)));
     }
 
     public int getIdEmployee() {
+        return idEmployee.get();
+    }
+
+    public IntegerProperty idEmployeeProperty() {
         return idEmployee;
     }
 
     public void setIdEmployee(int idEmployee) {
-        this.idEmployee = idEmployee;
+        this.idEmployee.set(idEmployee);
     }
 
     public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
         return name;
     }
 
-    public void setName(String nameame) {
-        this.name = nameame;
+    public void setName(String name) {
+        this.name.set(name);
     }
 
     public String getSurname() {
+        return surname.get();
+    }
+
+    public StringProperty surnameProperty() {
         return surname;
     }
 
     public void setSurname(String surname) {
-        this.surname = surname;
+        this.surname.set(surname);
     }
 
     public String getLogin() {
+        return login.get();
+    }
+
+    public StringProperty loginProperty() {
         return login;
     }
 
     public void setLogin(String login) {
-        this.login = login;
+        this.login.set(login);
     }
 
     public String getPassword() {
+        return password.get();
+    }
+
+    public StringProperty passwordProperty() {
         return password;
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password.set(password);
     }
 
     public String getAccess() {
+        return access.get();
+    }
+
+    public StringProperty accessProperty() {
         return access;
     }
 
     public void setAccess(String access) {
-        this.access = access;
+        this.access.set(access);
     }
 
     public int getSeed() {
+        return seed.get();
+    }
+
+    public IntegerProperty seedProperty() {
         return seed;
     }
 
     public void setSeed(int seed) {
-        this.seed = seed;
+        this.seed.set(seed);
     }
 
     public String getFullName() {
+        return fullName.get();
+    }
+
+    public StringProperty fullNameProperty() {
         return fullName;
     }
 
     public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "idEmployee=" + idEmployee +
-                ", nameame='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                ", access='" + access + '\'' +
-                ", seed=" + seed +
-                '}';
+        this.fullName.set(fullName);
     }
 }
